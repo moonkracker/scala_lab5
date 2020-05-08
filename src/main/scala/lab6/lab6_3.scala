a87a72e13ba9b90091a471e2f55d9207fa0c9d35
moon_racker <monster432max@live.com>
2020-05-08 23:03:35 +0300

    edt lab6_3

--- src/main/scala/lab6/lab6_3.scala
+++ src/main/scala/lab6/lab6_3.scala
@@ -1,14 +1,7 @@
 package lab6
 
 object lab6_3 {           //На основе List[String] сгенерировать строку, сконкатенированную через ' ' из тех подстрок List, которые начинаются с заглавной буквы.
-  def isAlphabetic(s: String): Boolean = s == s.toSeq.sorted.unwrap
-
   def concatination(list: List[String]): String ={
-    val tmp = list.filter(s => s.charAt(0).isUpper )
-    tmp.reduceLeft(_+" "+_)
-  }
-
-  def main(args: Array[String]): Unit = {
-
+    list.filter(s => s.charAt(0).isUpper ).reduceLeft(_+" "+_)
   }
 }
